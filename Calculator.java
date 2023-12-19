559849162c4ebedef58293623576203a92f25702
maliaaska <asmilewski@gmail.com>
2023-12-19 18:58:23 +0100

    first commit

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package com.milewskiarkadiuszmodul8.asmilewskiModul8;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        double result;
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+    public double dodawanie(double x, double y) {
+        double v = x + y;
+        return v;
+    }
+}
+
