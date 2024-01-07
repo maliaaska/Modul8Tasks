15f56127d81f0e060340f27bddc7d7da2d38fb12
maliaaska <asmilewski@gmail.com>
2024-01-07 13:57:26 +0100

    getters i setters added and methods as well

--- Rectangle.java
+++ Rectangle.java
@@ -16,18 +16,54 @@ public class Rectangle {
         this.positionY = positionY;
     }
 
-    public double getHeight() {
-        return height;
-    }
     public void Area(double height, double width) {
         this.height = height;
         this.width = width;
     }
     public double calculateArea() { return height * width; };
-    public void move(int x, int y) {};
-    public void containsPoint(Point p) {
-
+    public void move(int x, int y) {
+        setPositionX(positionX + x);
+        setPositionY(positionY + y);
+    };
+    public boolean containsPoint(Point p) {
+        return p.getX() >= positionX && p.getX() <= positionX + width &&
+                p.getY() >= positionY && p.getY() <= positionY + height;
     }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        if(height > 0 ) {    //Zakladamy ze height musi być wieksze niż zero zeby utworzyć plansze
+            this.height = height;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width > 0) {
+            this.width = width;
+        }
+    }
+    public int getPositionX() {
+        return positionX;
+    }
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+
+
 
 
 
