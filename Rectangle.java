68ff4d026748b723a20d101dd7373bb4da8f5d96
maliaaska <asmilewski@gmail.com>
2024-01-07 13:05:19 +0100

    The rectangle methods done

--- Rectangle.java
+++ Rectangle.java
@@ -7,5 +7,29 @@ public class Rectangle {
     private int positionX;
     private int positionY;
 
-    
+
+    //Konstruktor z podanymi współrzednymi
+    public Rectangle(double height, double width, int positionX, int positionY) {
+        this.height = height;
+        this.width = width;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public void Area(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+    public double calculateArea() { return height * width; };
+    public void move(int x, int y) {};
+    public void containsPoint(Point p) {
+
+    }
+
+
+
 }
+
