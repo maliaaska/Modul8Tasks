010a0aaa866f9f382701a9bf7e0c128f79647ad6
maliaaska <asmilewski@gmail.com>
2024-02-13 18:30:59 +0100

    the calculator processing amended

--- Calculator.java
+++ Calculator.java
@@ -3,56 +3,45 @@ package com.milewskiarkadiuszmodul8.asmilewskiModul8;
 import java.util.Scanner;
 
 public class Calculator {
+    public double x;
+    public double y;
+    public double result;
+
+    public Calculator(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void dodawanie(double x, double y) {
+        this.result = x + y;
+        System.out.println("Wynik dodawania to " + result);
+    }
+
+    public double odejmowanie(double x, double y) {
+        this.result = x - y;
+        System.out.println("Wynik odejmowania to " + result);
+        return result;
+    }
+
+    public double mnożenie(double x, double y) {
+        this.result = x * y;
+        System.out.println("Wynik Mnożenia to " + result);
+        return result;
+    }
+
+    public double dzielenie (double x, double y) {
+        this.result = x / y;
+        System.out.println("Wynik dzielenia to " + result);
+        return result;
+    }
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Wybierz działanie jakie chcesz wykonac (Dodawanie, odejmowanie, mnożenie, dzielenie");
-        String dzialanie = scanner.nextLine();
+        Calculator calculator = new Calculator(5,10);
 
+        calculator.dodawanie(5,7);
+        calculator.odejmowanie(20, 5);
+        calculator.mnożenie(3,8);
+        calculator.dzielenie(24,3);
 
-        if (dzialanie == null || dzialanie.isEmpty()) {
-            System.out.println("Nie wybrałeś poprawnego dzialania");
-        }
-
-
-        System.out.println("Podaj pierwszą liczbę");
-        double x = scanner.nextDouble();
-
-        System.out.println("Podaj drugą liczbę");
-        double y = scanner.nextDouble();
-
-        assert dzialanie != null;
-        switch (dzialanie) {
-            case "dodawanie" -> dodawanie(x, y);
-            case "odejmowanie" -> odejmowanie(x, y);
-            case "mnożenie" -> mnożenie(x, y);
-            case "dzielenie" -> dzielenie(x, y);
-        }
-
-    }
-
-    public static double dodawanie(double x, double y) {
-        double result = x + y;
-        System.out.println(result);
-        return result;
-    }
-
-    public static double odejmowanie(double x, double y) {
-        double result = x - y;
-        System.out.println(result);
-        return result;
-    }
-
-    public static double mnożenie(double x, double y) {
-        double result = x * y;
-        System.out.println(result);
-        return result;
-    }
-
-    public static double dzielenie (double x, double y) {
-        double result = x / y;
-        System.out.println(result);
-        return result;
     }
 }
 
